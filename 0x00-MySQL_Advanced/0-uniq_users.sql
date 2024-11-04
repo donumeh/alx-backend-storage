8a2b6c69d1483a994e9d25817934f0f6154a670f
Donald <donumeh54@gmail.com>
2024-11-04 09:54:53 +0100

    create table users

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,11 @@
+-- We are all unique
+-- Creates a table users
+
+-- CREATE DATABASE IF NOT EXISTS holberton;
+
+-- USE holberton;
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
