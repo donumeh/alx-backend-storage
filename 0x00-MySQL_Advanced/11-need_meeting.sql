e119398607994d2a30f0572d3452a447e0c996fd
Donald <donumeh54@gmail.com>
2024-11-06 14:50:53 +0100

    Create a view need_something that lists all students with some consitions

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,7 +2,7 @@
 -- all students that have a score
 
 CREATE VIEW need_meeting
-	AS SELECT * FROM students
+	AS SELECT name FROM students
 	WHERE score < 80 AND 
 		(last_meeting IS NULL 
 			OR (YEAR(last_meeting) - YEAR(CURDATE())) > 1);
