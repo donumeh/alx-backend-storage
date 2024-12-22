8a30314237f1268658e9e5d6bad91abce05be463
Donald <donumeh54@gmail.com>
2024-12-22 04:19:25 +0100

    update topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+Python function that chages all topics of a school doc
+"""
+
+def update_topics(mongo_collection, name, topics):
+    """
+    update_topics: changes all topcis of a school document
+
+    Args:
+        mongo_collection: the mongo collection
+        name: the key value to change
+        topics: the new values of key
+
+    Return: None
+    """
+
+    mongo_collection.update_one({"name":name}, {"$set": {"topics": topics}})
+
