baa55605a97c2630d2cef0d7f1203ca9a66ab725
Donald <donumeh54@gmail.com>
2024-12-22 12:47:01 +0100

    log stats from mongodb

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,16 +6,27 @@ Python script that provides some stats about Nginx logs stored in MongoDB
 
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-nginx = client.logs.nginx
 
-method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+def main():
+    """
+    main: print formatted details about nginx logs
+    """
 
-print("{} logs".format(len(list(nginx.find()))))
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx = client.logs.nginx
 
-for mthd in method:
-    print("\tmethod {}: {}"
-          .format(mthd, len(list(nginx.find({"method": mthd})))))
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-print("{} status check"
-      .format(len(list(nginx.find({"method": "GET", "path": "/status"})))))
+    print("{} logs".format(len(list(nginx.find()))))
+
+    for mthd in method:
+        print("\tmethod {}: {}"
+              .format(mthd, len(list(nginx.find({"method": mthd})))))
+
+    print("{} status check"
+          .format(
+              len(list(nginx.find({"method": "GET", "path": "/status"}))))
+          )
+
+
+main()
